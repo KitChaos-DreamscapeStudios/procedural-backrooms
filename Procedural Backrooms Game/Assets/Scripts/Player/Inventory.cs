54e722b8142ccfb59758bf3951e9ed9762995397
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-04-05 11:33:23 -0400

    Adjusted bloom, added more basic inventory functions

--- Procedural Backrooms Game/Assets/Scripts/Player/Inventory.cs
+++ Procedural Backrooms Game/Assets/Scripts/Player/Inventory.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public List<Item> Items;
+    public Item HandItem;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +15,16 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(1) && HandItem.isHoldable)
+        {
+            HandItem.UseHeld();
+        }   
     }
 }
 public abstract class Item : MonoBehaviour
 {
     public bool isHoldable;
     public bool isUsable;
-    public abstract void Use();
+    public abstract void UseInInventory();
+    public abstract void UseHeld();
 }
