d29c10a81006b9bd045c8f0fa19fbb05e9a8dd09
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-04-11 13:00:29 -0400

    Set up tables
    I like this new method more, and will stick with it
    
    I'm going to add an entity next

--- Procedural Backrooms Game/Assets/Scripts/WallLockedObject.cs
+++ Procedural Backrooms Game/Assets/Scripts/WallLockedObject.cs
@@ -7,25 +7,31 @@ public class WallLockedObject : MonoBehaviour
     public float Height;
     public string ForDirect;
     float Elap;
+    public float Rarity;
     // Start is called before the first frame update
     void Start()
     {
-       
-        Vector3 fordir = transform.right;
-        switch (ForDirect)
+
+        // Vector3 fordir = transform.right;
+        // switch (ForDirect)
+        // {
+        //     case "Forward":
+        //         fordir = transform.forward;
+        //         break;
+        //     case "Right":
+        //         fordir = transform.right;
+        //         break;
+
+        // }
+        //// transform.eulerAngles = new Vector3(0, Rotations[Random.Range(0, Rotations.Count)], 0);
+        // var hit = new RaycastHit();
+        // Physics.Raycast(transform.position + fordir, fordir, out hit);
+        // transform.position = hit.point;
+        var rand = Random.Range(0, 100);
+        if(rand < Rarity)
         {
-            case "Forward":
-                fordir = transform.forward;
-                break;
-            case "Right":
-                fordir = transform.right;
-                break;
-            
+            Destroy(gameObject);
         }
-       // transform.eulerAngles = new Vector3(0, Rotations[Random.Range(0, Rotations.Count)], 0);
-        var hit = new RaycastHit();
-        Physics.Raycast(transform.position + fordir, fordir, out hit);
-        transform.position = hit.point;
       
     }
 
