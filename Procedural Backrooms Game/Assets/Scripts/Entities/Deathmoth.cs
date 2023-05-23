a61590393ab066184d1365b51dfed0f223644080
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-05-23 13:29:34 -0400

    Continue to try and make moths
    I WILL DO IT LATER AUGHHHH

--- Procedural Backrooms Game/Assets/Scripts/Entities/Deathmoth.cs
+++ Procedural Backrooms Game/Assets/Scripts/Entities/Deathmoth.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+
 public class Deathmoth : Damager
 {
     public bool DetectedPlayer;
@@ -10,8 +10,6 @@ public class Deathmoth : Damager
     public LayerMask NotMonster;
     public Animator anim;
     bool Visible;
-    
-    //public NavMeshAgent agent;
     public enum State 
     {
         Resting,
@@ -22,9 +20,7 @@ public class Deathmoth : Damager
     // Start is called before the first frame update
     void Start()
     {
-        //agent = GetComponent<NavMeshAgent>();
         Player = GameObject.Find("Player");
-        //agent.enabled = false;
     }
     private void Update()
     {
@@ -42,16 +38,15 @@ public class Deathmoth : Damager
             PlayTimer += Time.deltaTime;
             if(PlayTimer >= 3)
             {
-               
                 PlayTimer = 0;
                 state = State.Flying;
             }
         }
         if(state == State.Flying)
         {
-            transform.position += transform.forward.normalized/6;
+            transform.position += transform.forward.normalized/3;
             Vector3 targ = Player.transform.position;
-
+          
 
             Vector3 objectPos = transform.position;
             targ.x = targ.x - objectPos.x;
@@ -63,20 +58,10 @@ public class Deathmoth : Damager
 
 
             //targ.y = targ.y - objectPos.y;
-            // PlayTimer += Time.deltaTime;
-            transform.forward = Vector3.Lerp(transform.forward, targ, 0.04f);
-          
-          //  GetComponent<BodyDof>
-
+            PlayTimer += Time.deltaTime;
+            transform.forward = Vector3.Slerp(transform.forward, targ, 0.1f);
+            
         }
-        if (Health <= 0)
-        {
-            Die();
-        }
-    }
-    void AddPositions()
-    {
-        //Positions.Add(Player.transform.position);
     }
 
     // Update is called once per frame
@@ -89,7 +74,7 @@ public class Deathmoth : Damager
     {
         
     }
-    public void OnCollisionEnter(Collision col)
+    new public void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.GetComponent<DamageSpear>())
         {
@@ -98,5 +83,12 @@ public class Deathmoth : Damager
         }
 
     }
-   
+    private void OnBecameInvisible()
+    {
+        Visible = false;
+    }
+    private void OnBecameVisible()
+    {
+        Visible = true;
+    }
 }
