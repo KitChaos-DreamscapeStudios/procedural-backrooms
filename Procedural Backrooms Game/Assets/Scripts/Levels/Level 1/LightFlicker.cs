415e1d53db7f9339935a4db6c58586422646c1dd
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-05-31 12:44:51 -0400

    Added Light Flashing
    Its very buggy rn, but its fine

---
+++ Procedural Backrooms Game/Assets/Scripts/Levels/Level 1/LightFlicker.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public ParticleSystem Burst;
+    public AudioSource FlickNoise;
+    public AudioSource BurstNoise;
+    public float t;
+    bool CanBurst;
+    float BurstTime;
+    public Light L;
+    bool IsDoFlick;
+    float BaseIntenisty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Burst = GetComponentInChildren<ParticleSystem>();
+        FlickNoise = GetComponent<AudioSource>();
+       // BurstNoise = GetComponentInChildren<AudioSource>();
+        L = GetComponent<Light>();
+        BaseIntenisty = L.intensity;
+      
+        var Rand = Random.Range(0, 5);
+        if(Rand == 3)
+        {
+            CanBurst = true;
+        }
+        BurstTime = Random.Range(20, 40);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanBurst)
+        {
+            t += Time.deltaTime;
+            if(t - Mathf.Round(t) < 0.05 && CanBurst)
+            {
+                //BurstNoise.Play();
+                var Rand = Random.Range(0, 30);
+                if(Rand == 5)
+                {
+                    //FlickNoise.Play();
+                    IsDoFlick = true;
+                }
+                else
+                {
+                    IsDoFlick = false;
+                }
+            }
+            if (t > BurstTime)
+            {
+                if(Physics.CheckSphere(transform.position, 3, 3))
+                {
+                    GameObject.Find("Player").GetComponent<PlayerStats>().TakeDamage(20, "Killed by Exploding Light");
+                }
+                L.intensity = BaseIntenisty;
+                BurstNoise.volume = 1;
+                BurstNoise.Play();
+                CanBurst = false;
+
+                Burst.Play();
+                L.intensity = 0;
+                Destroy(GetComponent<Light>());
+                Destroy(GetComponent<MeshRenderer>());
+                // Destroy(FlickNoise);
+                Destroy(gameObject, 0.5f);
+
+            }
+        }
+        
+        
+        if (IsDoFlick && CanBurst)
+        {
+            L.intensity = Mathf.Sin(t);
+        }
+        else
+        {
+            if (CanBurst)
+            {
+                L.intensity = BaseIntenisty;
+            }
+           
+        }
+    }
+}
